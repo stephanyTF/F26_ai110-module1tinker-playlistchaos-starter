f941eead57840c7a3037a0f5cb15eefd257d39d4
stephanyTF <stephany.lam@codepath.org>
2026-09-11 15:56:12 -0700

    fix(NEED REVISIT): switched order but same outcome?

--- playlist_logic.py
+++ playlist_logic.py
@@ -73,7 +73,7 @@ def classify_song(song: Song, profile: Dict[str, object]) -> str:
     is_hype_keyword = any(k in genre for k in hype_keywords)
     is_chill_keyword = any(k in title for k in chill_keywords)
 
-    if genre == favorite_genre or energy >= hype_min_energy or is_hype_keyword:
+    if energy >= hype_min_energy or genre == favorite_genre  or is_hype_keyword: # PART 2 BUG 1 FIX: switched order 
         return "Hype"
     if energy <= chill_max_energy or is_chill_keyword:
         return "Chill"
